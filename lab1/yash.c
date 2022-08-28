fa06ce9f36ffec58bc7838849c7519726d83a40b
brandnewtothis <jflotildes@utexas.edu>
2022-08-28 17:13:06 -0500

    initial commit

---
+++ lab1/yash.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+
+int main(){
+	
+
+   printf("Hello World\n");
+
+   while(1){
+  
+      // 0. Register Signal Handlers
+
+      // 1. Print prompt
+      //printf(">");
+      
+
+
+      // 2. Grab and parse input - remove newLine modifier (\n)  
+      //each token will be no more than 30 characters
+  
+      //3. Check for job control Tokens
+      
+
+
+      // 4. Determine number of children processes to create (# times to call fork() ) 
+         
+
+      // 5. execute commands using execvo or execlp
+      
+
+      // 6. Other commands for job stuff
+
+   }	  
+
+}
+
